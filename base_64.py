ff859e2730386df829ba6439a8cc79d37c57ec1e
Mubarak153 <B00128338@mytudublin.ie>
2021-10-07 07:45:53 -0400

    hi

--- base_64.py
+++ base_64.py
@@ -1,4 +1,4 @@
 import base64
 hexadecimal_str = '72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf'
 hex_ee = bytes.fromhex(hexadecimal_str)
-print (base64.b64encode(hex_ee))
+print (base64.b64encode(hex_ee))ff
