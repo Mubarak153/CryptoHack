d2826f1c580902703e853b96adabc006fea39520
Mubarak153 <B00128338@mytudublin.ie>
2022-01-08 16:23:25 -0500

    Crypto

--- You_Either_Know_XOR_or_you_dont.py
+++ You_Either_Know_XOR_or_you_dont.py
@@ -1,6 +1,5 @@
 from pwn import xor
 
 a = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
-key = xor(a[:7], 'crypto{') + xor(a[-1], '}') # we know first 7 bytes and the last 1
-# the first part is 'myXORke' and the last is 'y'
+key = xor(a[:7], 'crypto{') + xor(a[-1], '}') 
 print(xor(a, key)) 
